b8ed7a7febd4dd5dce8a428b788131c38d4121c8
jehanpark <5071pjh@naver.com>
2024-09-25 20:56:18 +0900

    0925update

---
+++ 240925/sns-flatform/src/App.tsx
@@ -0,0 +1,80 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { createGlobalStyle } from "styled-components";
+import { auth } from "./firebase";
+import reset from "styled-reset";
+import Layout from "./compornents/Layout";
+import Home from "./routes/Home";
+import Profile from "./routes/Profile";
+import Login from "./routes/Login";
+import CreateAccount from "./routes/CreateAccount";
+import LoadingScreen from "./compornents/LoadingScreen";
+import ProtectedRoute from "./compornents/ProtectedRoute";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: 
+    (<ProtectedRoute>
+      <Layout />
+    </ProtectedRoute>),
+    children: [
+      {
+        path: "",
+        element: 
+        (<ProtectedRoute>
+          <Home />
+        </ProtectedRoute>),
+      },
+      {
+        path: "profile",
+        element: 
+        (<ProtectedRoute>
+          <Profile />
+        </ProtectedRoute>),
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/create-account",
+    element: <CreateAccount />,
+  },
+]);
+
+const GlobalStyles = createGlobalStyle`
+${reset}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #000;
+  color: #fff;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+}
+`;
+
+function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const init = async () => {
+    await auth.authStateReady();
+    await setIsLoading(false);
+  };
+  useEffect(() => {
+    init();
+  }, []);
+  return (
+    <>
+      <GlobalStyles />
+      {isLoading ? <LoadingScreen /> : <RouterProvider router={router} />}
+    </>
+  );
+}
+
+export default App;
---
+++ 240925/sns-flatform/src/routes/Home.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { auth } from "../firebase";
+
+const Home = () => {
+  const navigate = useNavigate();
+  const logout =()=>{
+    auth.signOut();
+    navigate("/login")
+  }
+
+  return ( 
+     <h1>
+    <button onClick={logout}>logout</button>
+    </h1>
+    )
+};
+
+export default Home;
